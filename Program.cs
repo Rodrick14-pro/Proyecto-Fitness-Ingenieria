3150f33bfe2691eb541f22764f8e3c71c9ad661a
Rodrigo Camez <rodrigocamez1@gmail.com>
2026-02-11 13:21:07 -0700

    Convert folder to a formal .NET Console Project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 1. Creamos el objeto (el procesador de tarjetas)
+        CardPayment myProcessor = new CardPayment();
+
+        // 2. Probamos una cantidad pequeña (Entrada de datos 1)
+        Console.WriteLine("--- Test 1: Small Amount ---");
+        myProcessor.ProcessPayment(250.00);
+
+        Console.WriteLine(); // Espacio en blanco
+
+        // 3. Probamos una cantidad que exceda el límite (Entrada de datos 2)
+        Console.WriteLine("--- Test 2: Large Amount ---");
+        myProcessor.ProcessPayment(1500.00);
+    }
+}
